bfab03e7d773a925439c4d34c2b95a71a7733e2a
mwiko <mwikosinyangwe@gmail.com>
2025-05-30 17:51:48 +0200

    added product detail func

--- api/serializers.py
+++ api/serializers.py
@@ -19,3 +19,23 @@ class ProductSerializer(serializers.ModelSerializer):
                     "Price must be greater than 0."
                 )
             return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    class Meta:
+        model = OrderItem
+        fields = ('product', 'quantity')
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+    
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+    class Meta:
+        model = Order
+        fields = ('order_id', 'created_at', 'user', 'status','items','total_price',)
+    
+    
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer, OrderItemSerializer
+from api.models import Product, Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -9,11 +9,18 @@ from rest_framework.decorators import api_view
 def product_list(request):
     products = Product.objects.all()
     seriallizer = ProductSerializer(products, many=True)
-
-
     return Response(seriallizer.data)
+
+
 @api_view(['GET'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     seriallizer = ProductSerializer(product)
+    return Response(seriallizer.data)
+
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    seriallizer = OrderSerializer(orders, many=True)
     return Response(seriallizer.data)
