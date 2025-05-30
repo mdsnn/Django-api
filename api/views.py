8e37c9022fd0f0a23896915ce36511f191eff051
mwiko <mwikosinyangwe@gmail.com>
2025-05-30 16:38:53 +0200

    added product detail func

--- api/views.py
+++ api/views.py
@@ -1,10 +1,19 @@
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from api.serializers import ProductSerializer
 from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     seriallizer = ProductSerializer(products, many=True)
-    return JsonResponse({
-        'data': seriallizer.data
-    })
+
+
+    return Response(seriallizer.data)
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    seriallizer = ProductSerializer(product)
+    return Response(seriallizer.data)
